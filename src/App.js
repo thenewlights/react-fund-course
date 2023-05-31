49c084348a60b839d2154c33efe727c9bda8b582
eugene <evgenybbwd@gmail.com>
2023-05-31 22:35:31 +0300

    Small changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Counter from "./components/Counter"
 import ClassCounter from "./components/ClassCounter"
+import './styles/App.css'
+import PostItem from "./components/PostItem";
 
 function App() {
 
   return (
     <div className="App">
-      <ClassCounter />
+      <PostItem post={{ id: 1, title: "Javascript", body: 'Description' }} />
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const PostItem = (props) => {
+    console.log(props);
+    return (
+        <div className="post">
+            <div className="post__content">
+                <strong>{props.post.id}. {props.post.title}</strong>
+                <div>{props.post.body}</div>
+            </div>
+            <div className="post__btns">
+                <button>Удалить</button>
+            </div>
+        </div>
+    );
+}
+
+export default PostItem;
